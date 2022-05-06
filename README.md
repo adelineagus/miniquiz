4b930dd97fd4e16e75cc306e61c570f2877de5ba
Adeline <adelineaguspranoto@gmail.com>
2022-05-05 19:15:50 -0700

    update css and comments

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # miniquiz
+
+
+THis miniquiz project's mainly focuses on utilizing web APIs attributes. Variables in javascript are mostly created by grabbing elements from html, although some variables are created within javascript itself (creating choices buttons, setting up questions variable, etc.). Some elements are assigned to some events through clicking, submitting, etc. This project also utilizes local storage to store user initials and scores - when page is refreshed, user's initials and scores will still be stored and won't get deleted/cleared. 
+
+Achievements:
+1. When start button is clicked, timer on the upper left side will start ticking down and quiz page with question and choices will be presented
+2. When choice button is clicked/answered, statement of true/false will be presented and another question will popped out
+3. If user answers incorrectly, time will be substracted by 10 from the timer
+4. When timer reaches 0 or users have answered all questions, the game is over. 
+5. When the game is over, user is presented with correct amount summary and form to enter initials
+6. Once  user submit initials, user is presented with past records/scores with initials and option to play again or clear the scores
+7. when user click "view scores" button on the upper right side, user will be presented with score page (containing score records, play button and clear button)
+
+
+Screenshot: 
+
+Website:
--- script.js
+++ script.js
@@ -2,20 +2,59 @@
 var viewScores=document.getElementById('view-scores');
 var timerElement=document.querySelector(".timer-countdown");
 var timer;
-var timerCount=10;
+var timerCount=50;
 var correctAmount=0;
 var questionNum=0;
 var questions=[
     {
-        question: "what's my name?",
-        answers: ['Adeline','Adel','Line'],
-        correctAnswer: 'Adeline'
+        question: "What does Sayonara mean?",
+        answers: ['Hello','Goodbye','Thankyou'],
+        correctAnswer: 'Goodbye'
     },
+
     {
-        question: "how old is she?",
-        answers: ['15','10','25'],
-        correctAnswer: '25'
+        question: "What was the first soft drink taken into space?",
+        answers: ['Coca-Cola','Pepsi','Fanta'],
+        correctAnswer: 'Coca-Cola'
+    },
+
+    {
+        question: "How many hearts does an octopus have?",
+        answers: ['2','3','4'],
+        correctAnswer: '3'
+    },
+
+    {
+        question: "What percentage of water does a watermelon have?",
+        answers: ['92%','95%','99%'],
+        correctAnswer: '92%'
+    },
+
+    {
+        question: "What is the rarest M&M color?",
+        answers: ['Red','Green','Brown'],
+        correctAnswer: 'Brown'
+    },
+
+    {
+        question: "What is the hottest planet in the solar system?",
+        answers: ['Mercury','Mars','Venus'],
+        correctAnswer: 'Venus'
+    },
+
+    {
+        question: "What country has the most natural lakes?",
+        answers: ['Canada','United States','China'],
+        correctAnswer: 'Canada'
+    },
+
+    {
+        question: "How many bones do we have in an ear?",
+        answers: ['0','3','5'],
+        correctAnswer: '3'
     }
+
+
 ]
 var welcomeContainer=document.getElementById('welcome-page')
 var startContainer=document.getElementById('start');
@@ -27,10 +66,10 @@ var resultspageContainer=document.getElementById('results-page');
 var resultsContainer=document.getElementById('results');
 var initialsInput= document.getElementById('initials-input');
 var initialsForm= document.getElementById('initials-form');
-var userInitials=[];
+var userinitialsScores=[];
 var scorepageContainer=document.getElementById('score-page');
 var scoreContainer= document.getElementById('score');
-var playButton=document.getElementById('play-again');
+var playButton=document.getElementById('play-game');
 var clearButton=document.getElementById('clear-scores');
 
 
@@ -40,40 +79,50 @@ scorepageContainer.style.display='none';
 
 
 //stating all events
+
+//when view scores button is clicked, timer is stopped/cleared, showScores function is called
 viewScores.addEventListener('click',function(){
     clearInterval(timer);
     showScores();
 })
 
+//when start button is clicked, timerStart and quizContent function is called
 startContainer.addEventListener('click',function(){
     timerStart();
-    quizContent(questions,questionNum);
+    quizContent(questions, questionNum);
 })
 
+//when choice button is clicked, user's choice will be recorded in var and used in storeAnswer function, questionNum is add by 1, clearChoice function is called.
 choicesContainer.addEventListener('click',function(event,userChoice){
     var userChoice=event.target;
     storeAnswer(userChoice);
     questionNum++;
     clearChoices();
     
+    //if question num is less than length of var questions (number of questions in var), quizContent function will be called, 
     if(questionNum<questions.length){
+        //setTimeout(quizContent,500)
         quizContent();
-    } else {
+    
+    } else { // otherwise, resultsSection function will be called
         clearInterval(timer);
-        resultsSection();
+        setTimeout(resultsSection,600);
     }
 })
 
+// when form is filled out and user clicked enter/submit, scoreGenerator and showScores functions are called
 initialsForm.addEventListener("submit", function(event){
     event.preventDefault();
     scoreGenerator();
     showScores();
 })
 
+//when play buttonis clicked, reset function is called
 playButton.addEventListener('click', function(){
     reset();
 })
 
+//when clear button is clicked, clearscorePage and clearScores functions are called
 clearButton.addEventListener('click',function(){
     clearscorePage();
     clearScores();
@@ -81,34 +130,48 @@ clearButton.addEventListener('click',function(){
 
 
 //function details
+
+//setting up timer countdown
 function timerStart (){
     timer=setInterval(function(){
         timerCount--;
         timerElement.textContent=timerCount;
+
+        //if time reaches zero (or if timer is less than 0),
+            //timer will be cleared and timer countdown will show zero, 
+            //resultsSection function will be called
         if(timerCount<=0){
             clearInterval(timer);
+            timerCount=0;
             timerElement.textContent='0';
-            quizContainer.style.display='none';
-            resultsSection();
+            setTimeout(resultsSection,600);
         }
-    },1000);
+    },1000); //1000 ms= 1 s
 }
 
+
+//displaying quiz page and make welcome page disappear
+    //display questions and answer choices on screen
 function quizContent(){ 
     welcomeContainer.style.display='none';
     quizContainer.style.display='inline-block';
     questionContainer.innerText=questions[questionNum].question;
+    questionContainer.setAttribute('style', 'font-size:50px; padding:20px; text-align:center');
+    
     for (var i=0; i<3;i++){
         var choiceButton= document.createElement('button');
         choiceButton.innerText= questions[questionNum].answers[i];
+        choiceButton.setAttribute('style', 'background-color: black; color:white; width: 200px; height:50px; font-size: 25px');
         choiceButton.setAttribute("answer", questions[questionNum].answers[i]);
         choicesContainer.appendChild(choiceButton);
     }
 }
 
+//checking whether user's choice/answer matches with the correct answer 
 function storeAnswer(userChoice){
-    statementContainer.style.display='block';
+    statementContainer.style.display='inline-block';
 
+      //display statement: true /false
     if (userChoice.getAttribute('answer')===questions[questionNum].correctAnswer){
         statementContainer.innerHTML="True!";
         setTimeout(function(){
@@ -120,33 +183,41 @@ function storeAnswer(userChoice){
         setTimeout(function(){
             statementContainer.style.display='none';
         },500);
-        timerCount=timerCount-5;
+
+        //timer count will be substracted by 10 whenever userchoice is wrong
+        timerCount=timerCount-10;
     }
 }
 
+//reset/delete all choices
 function clearChoices(){
     while(choicesContainer.firstChild){
         choicesContainer.removeChild(choicesContainer.lastChild);
     }
 }
 
+//make quiz page disappear and display result page
+    //result page will contain statements on how many correct amount
 function resultsSection(){
     quizContainer.style.display='none';
-    setTimeout(function(){
-        resultspageContainer.style.display='inline-block';
-    },500)
+    resultspageContainer.style.display='inline-block';
     resultsContainer.innerHTML= "Correct Amount : " + correctAmount;
+    resultsContainer.setAttribute('style', 'font-style: bold; font-size: 25px;')
 }
 
+//take initials from user and combine it with his/her scores
+    //retrieve past record and store new records
 function scoreGenerator(){
     var initials= initialsInput.value.trim();
     var initialsScore= initials + " - " + ((correctAmount).toString());
     getScores();
-    userInitials.push(initialsScore);
+    userinitialsScores.push(initialsScore);
     storeScores();
     initialsInput.value="";
 }
 
+//display score page and make welcome, quiz, and results page disappear
+    //show list of scores from current and past record
 function showScores(){
     welcomeContainer.style.display='none';
     quizContainer.style.display='none';
@@ -154,42 +225,47 @@ function showScores(){
     scorepageContainer.style.display='flex';
     getScores();
     scoreContainer.innerHTML="";
-    for(var i=0; i<userInitials.length;i++){
+    for(var i=0; i<userinitialsScores.length;i++){
         var li= document.createElement("li");
-        li.textContent=userInitials[i];
+        li.textContent=userinitialsScores[i];
         scoreContainer.appendChild(li);
     }
 }
 
+//retrieve saved/past scores from local storage
 function getScores(){
     var storedScores= JSON.parse(localStorage.getItem("scores"));
     if(storedScores !== null){
-        userInitials=storedScores;
+        userinitialsScores=storedScores;
     }
 }
 
+//store initials and scores in local storage
 function storeScores(){
-    localStorage.setItem("scores", JSON.stringify(userInitials));
+    localStorage.setItem("scores", JSON.stringify(userinitialsScores));
 }
 
+//reset variables to initial value and display welcome page
 function reset(){
     timer;
-    timerCount=10;
+    timerCount=50;
     correctAmount=0;
     questionNum=0;
     clearChoices();
     scorepageContainer.style.display='none';
-    welcomeContainer.style.display='inline-block';
+    welcomeContainer.style.display='flex';
 }
 
+//clear contents on score page
 function clearscorePage(){
     while(scoreContainer.firstChild){
         scoreContainer.removeChild(scoreContainer.lastChild);
     }
 }
 
+//clear local storage by set initial and scores variable to an empty array and save it to local storage
 function clearScores(){
-    userInitials=[];
+    userinitialsScores=[];
     storeScores();
 }
 
